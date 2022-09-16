066d6fa88b2986b7b70bf797deb02753153c6700
Jared <jared.wood002@gmail.com>
2022-09-16 14:36:55 -0500

    Added Initial File reading.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 mod functions;
 
+use std::fs;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("src/data.txt").unwrap();
+    let reader = BufReader::new(file);
+
+    for (index,line) in reader.lines().enumerate() {
+        let line = line.unwrap();
+        print!("{}. {}  ",index + 1, line);
+    }
 }
