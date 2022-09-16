301f2003f0b1773fba184bb050007fd58464724d
logan <loganf0101@gmail.com>
2022-09-16 14:51:38 -0500

    implemented code from Jared branch, allows user to read text line by line and input that into a vector

--- day_1/src/functions.rs
+++ day_1/src/functions.rs
@@ -1,11 +1,22 @@
 // Libraries
-use std::env;
 use std::fs;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 pub fn read_file(){
 
-    let contents = fs::read_to_string("src/input.txt")
-        .expect("Should have been able to read the file");
+    let file = File::open("src/input.txt").unwrap();
+    let reader = BufReader::new(file);
 
-    println!("With text:\n{contents}");
+    let mut depth_measurements = vec![0i32];
+
+    for (index,line) in reader.lines().enumerate() {
+        let line = line.unwrap();
+        let num = line.parse().unwrap();
+        depth_measurements.push(num);
+    }
+
+    println!("{}", depth_measurements[2]);
 }
+
+// Find a way to return the vector so when it's called in a calculating function, it can be used in there dynamically 
