6cc2d5e81a1a128439a8e53381ed58ec906ace71
logan <loganf0101@gmail.com>
2022-09-16 17:27:21 -0500

    Changed for loop condition again\

--- day_1/src/functions.rs
+++ day_1/src/functions.rs
@@ -7,7 +7,7 @@ pub fn read_file() -> Vec<i32>{
     let file = File::open("src/input.txt").unwrap();
     let reader = BufReader::new(file);
     let mut depth_measurements:Vec<i32> = vec![0i32]; // Vector to hold read file data
-    for (index,line) in reader.lines().enumerate() {
+    for (_index,line) in reader.lines().enumerate() {
         let line = line.unwrap();
         let num = line.parse().unwrap();
         depth_measurements.push(num);
@@ -17,9 +17,9 @@ pub fn read_file() -> Vec<i32>{
 
 pub fn calc_measurements(){
     let mut increments:i32 = 0;
-    let mut depth_measurements: Vec<i32> = read_file(); // Call function read_file and input that vector to the new vector
+    let depth_measurements: Vec<i32> = read_file(); // Call function read_file and input that vector to the new vector
     for i in 0..depth_measurements.len()-1{ // Goes out of bounds without the -1
-        if(depth_measurements[i+1] > depth_measurements[i]){increments+=1;} // Increment if the value is greater
+        if depth_measurements[i+1] > depth_measurements[i] {increments+=1;} // Increment if the value is greater
     }
     println!("Total change in measurements: {increments}");
 } // End of calc_measurements
