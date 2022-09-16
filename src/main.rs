5dacaf69605eac46a828f9beb3ddc1428bcc13de
Jared <jared.wood002@gmail.com>
2022-09-16 14:46:00 -0500

    Added putting data file input into a vector array called depth_measurements.

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,16 @@ fn main() {
     let file = File::open("src/data.txt").unwrap();
     let reader = BufReader::new(file);
 
+    let mut depth_measurements = vec![0i32];
+
     for (index,line) in reader.lines().enumerate() {
         let line = line.unwrap();
-        print!("{}. {}  ",index + 1, line);
+        let num = line.parse().unwrap();
+        depth_measurements.push(num);
     }
+
+    println!("{}", depth_measurements[2]);
+
+
 }
+
