7233767f430d02b229d5500e7300817aa935c948
logan <loganf0101@gmail.com>
2022-09-16 17:21:31 -0500

    cleaned up file and changed condition, check if 2nd index is greater than first, rather than vise versa

--- day_1/src/functions.rs
+++ day_1/src/functions.rs
@@ -4,29 +4,22 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 pub fn read_file() -> Vec<i32>{
-
     let file = File::open("src/input.txt").unwrap();
     let reader = BufReader::new(file);
-
-    let mut depth_measurements:Vec<i32> = vec![0i32];
-
+    let mut depth_measurements:Vec<i32> = vec![0i32]; // Vector to hold read file data
     for (index,line) in reader.lines().enumerate() {
         let line = line.unwrap();
         let num = line.parse().unwrap();
         depth_measurements.push(num);
     }
     return depth_measurements;
-}
+} // End of read_file (returns a vector)
 
 pub fn calc_measurements(){
     let mut increments:i32 = 0;
-    let mut depth_measurements: Vec<i32> = read_file();
-    for i in 0..depth_measurements.len()-1{
-        if(depth_measurements[i] > depth_measurements[i+1]){
-            increments+=1;
-        }
+    let mut depth_measurements: Vec<i32> = read_file(); // Call function read_file and input that vector to the new vector
+    for i in 0..depth_measurements.len()-1{ // Goes out of bounds without the -1
+        if(depth_measurements[i+1] > depth_measurements[i]){increments+=1;} // Increment if the value is greater
     }
     println!("Total change in measurements: {increments}");
-}
-
-// Find a way to return the vector so when it's called in a calculating function, it can be used in there dynamically 
+} // End of calc_measurements
